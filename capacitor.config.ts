600203fc638d2979b3acf5faa7f99978f041f43c
Aakansha Tyagi <navdeep@binmile.com>
2026-04-06 15:54:04 +0530

    live reload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myApp',
-  webDir: 'www'
+  webDir: 'www',
+ plugins: {
+    LiveUpdates: {
+      appId: '5bdf87e0',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
-
 export default config;
